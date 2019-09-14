b79fa6237959590f38cbde06189c823ae61a63df
anderskate <andrey.starozhilov@mail.ru>
2019-09-14 15:47:27 +0500

    create a new repository

---
+++ property/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Flat
+
+
+@admin.register(Flat)
+class FlatAdmin(admin.ModelAdmin):
+	search_fields = ['town', 'address', 'owner']
+	# fields = ('town', 'address', 'owner')
+
+
